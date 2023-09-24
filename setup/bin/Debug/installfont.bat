208867eaf5ae2de77fc95032d4306a88adb3efdc
dachao <dachaozhou@outlook.com>
2023-09-24 21:25:37 +0800

    添加installfont.bat

---
+++ setup/bin/Debug/installfont.bat
@@ -0,0 +1,7 @@
+echo Set objAP = CreateObject("wscript.shell")>font.vbs
+echo wscript.sleep 3000>>font.vbs
+echo objAP.SendKeys "%%{i}">>font.vbs
+echo wscript.sleep 3000>>font.vbs
+echo objAP.SendKeys "%%{F4}">>font.vbs
+start "" font.vbs
+"°Άΐο°Ν°ΝΖΥ»έΜε R.ttf"
